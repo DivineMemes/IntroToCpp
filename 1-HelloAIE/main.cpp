b47ba5eedf6104775ffd27981aa8d6e7890536be
s179053 <s179053@SEA-7FF209>
2017-08-14 12:46:06 -0700

    Add project files.

---
+++ 1-HelloAIE/main.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "Hello AIE! " << std::endl;
+	system("pause");
+
+	//mixtape
+	std::cout << "The Following is a tracklist for my mixtape. " << std::endl;
+	std::cout << "Track_1 (Memes.exe)" << std::endl;
+	std::cout << "Track_2 (Memes.txt)" << std::endl;
+	std::cout << "Track_3 (Memes.cpp)" << std::endl;
+	system("pause");
+	//When "i" is less than 5 it increases and prints out
+	for (int i = 0; i <= 5; i++)
+		std::cout << i << std::endl;
+	
+	system("pause");
+	//When 0 is less than 5 it increases and prints out
+	for(int v = 0; v <= 5; v++)
+	{ 
+		std::cout << v << std::endl;
+		
+		if (v == 5)
+		{
+			for (int o = 5; o > 0; o--)
+				std::cout << o << std::endl;
+			system("pause");
+		} 
+	}
+	std::cout << "SyntacticalErrors-A)" << std::endl;
+	std::cout << "Howdy AIE" << std::endl;
+	std::cout << "It's a fine day today as any!" << std::endl;
+	std::cout << "I really wish I had some potatoes..." << std::endl;
+	system("pause");
+
+	//I copy pasted stuff
+	std::cout << "SyntacticalErrors-B)" << std::endl;
+	std::cout << "Four score and seven years ago" << std::endl;
+	std::cout << "our fathers brought forth on this continent," << std::endl;
+	std::cout << "a new nation," << std::endl;
+	std::cout << "conceived in Libery, and dedicated to the proposition" << std::endl;
+	std::cout << "that all men are created equal." << std::endl;
+
+	std::cout << "Now we are engaged in a great civil war," << std::endl;
+	std::cout << "testing whether that nation, or any nation" << std::endl;
+	std::cout << "so conceived and so dedicated, can long endure." << std::endl;
+	std::cout << "SyntacticalErrors-B)" << std::endl;
+	std::cout << "Four score and seven years ago" << std::endl;
+	std::cout << "our fathers brought forth on this continent," << std::endl;
+	std::cout << "a new nation," << std::endl;
+	std::cout << "conceived in Libery, and dedicated to the proposition" << std::endl;
+	std::cout << "that all men are created equal." << std::endl;
+
+	std::cout << "Now we are engaged in a great civil war," << std::endl;
+	std::cout << "testing whether that nation, or any nation" << std::endl;
+	std::cout << "so conceived and so dedicated, can long endure." << std::endl;
+	system("pause");
+}
